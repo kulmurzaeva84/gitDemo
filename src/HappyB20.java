238bc919fe4a52b8fe5d6eb7d7e209499fd7652e
kulmurzaeva84 <kjika84@gmail.com>
2020-08-01 11:00:15 -0500

    another message from me:)

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,5 +1,7 @@
 public class HappyB20 {
     public static void main(String[] args) {
+
         System.out.println("I am happy today!");
+        System.out.println("Today is August 1st");
     }
 }
